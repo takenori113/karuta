921f19d0c5f6005826e36041baf1b6cafabb4baf
Takenori <takenori113@gmail.com>
2024-06-29 17:13:34 +0900

    読札の文章修正

--- src/karuta/karuta.ts
+++ src/karuta/karuta.ts
@@ -1,42 +1,42 @@
 export type KarutaCard = { sentence: string; image: string; voice: string };
 export const karutaCards: KarutaCard[] = [
   {
-    sentence: "生まれは、神田駿河台、わんぱく小僧の剛太郎",
+    sentence: "江戸っ子の　目を驚かす　築地のホテル",
     image: "/e.jpg",
     voice: "/e.m4a",
   },
   {
-    sentence: "生まれは、神田駿河台、わんぱく小僧の剛太郎",
+    sentence: "非業の死　悼む墓守　黒椿",
     image: "/hi.jpg",
     voice: "/hi.m4a",
   },
   {
-    sentence: "生まれは、神田駿河台、わんぱく小僧の剛太郎",
+    sentence: "ポウハタン　遣米使節が　乗った船",
     image: "/ho.jpg",
     voice: "/ho.m4a",
   },
   {
-    sentence: "生まれは、神田駿河台、わんぱく小僧の剛太郎",
+    sentence: "苦心した　通貨分析　フィラデルフィア",
     image: "/ku.jpg",
     voice: "/ku.m4a",
   },
   {
-    sentence: "生まれは、神田駿河台、わんぱく小僧の剛太郎",
+    sentence: "もてなしに　心身癒す　和光原",
     image: "/mo.jpg",
     voice: "/mo.m4a",
   },
   {
-    sentence: "生まれは、神田駿河台、わんぱく小僧の剛太郎",
+    sentence: "ネジ握る　小栗の眼に　迷いなく",
     image: "/ne.jpg",
     voice: "/ne.m4a",
   },
   {
-    sentence: "生まれは、神田駿河台、わんぱく小僧の剛太郎",
+    sentence: "レンガ積み　富岡製糸に　伝えられ",
     image: "/re.jpg",
     voice: "/re.m4a",
   },
   {
-    sentence: "生まれは、神田駿河台、わんぱく小僧の剛太郎",
+    sentence: "正装に　威儀を正して　批准書交換",
     image: "/se.jpg",
     voice: "/se.m4a",
   },
@@ -46,7 +46,7 @@ export const karutaCards: KarutaCard[] = [
     voice: "/u.m4a",
   },
   {
-    sentence: "生まれは、神田駿河台、わんぱく小僧の剛太郎",
+    sentence: "友好の　絆は深し　はまゆう山荘",
     image: "/yu.jpg",
     voice: "/yu.m4a",
   },
